69bd6a033f17c62953a1f56264314be3fe4f86d4
enjoyurrlty <enjoyurrlty@gmail.com>
2025-09-01 00:57:36 +0200

    Add 60 fps

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +16,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
