d50bb64d0403115220ff134833fd15391a179ec3
enjoyurrlty <enjoyurrlty@gmail.com>
2025-09-01 17:28:50 +0200

    add game over

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroid import Asteroid
@@ -32,6 +33,9 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                sys.exit("Game over!")
         for item in drawable:
             item.draw(screen)
         
